7d1e7e91cd2b0c808268cd426420774673b0ef33
valentinvisan <valentin.visan.vv@gmail.com>
2025-05-15 18:25:03 +0300

    initial setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	dial, err := amqp.Dial(connectionString)
+	if err != nil {
+		return
+	}
+	defer closer(dial)
+
+	fmt.Println("Connected to RabbitMQ")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Received an interrupt, stopping services")
+	closer(dial)
+}
+
+func closer(dial *amqp.Connection) {
+	if dial.IsClosed() {
+		return
+	}
+
+	if err := dial.Close(); err != nil {
+		panic(err)
+	}
 }
